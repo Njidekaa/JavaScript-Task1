6d7207223ec06410464cdfc45b4234254411e2d9
unknown <onyishiprecious73@gmail.com>
2022-06-22 16:53:38 +0100

    First JavaScript Codes

---
+++ script.js
@@ -0,0 +1,6 @@
+let myName = "Onyishi Precious Njideka";
+let height = "6ft Tall";
+let country = "Nigeria";
+
+
+alert("My name is " + myName + ".\n I am " + height + ".\n I am a citizen of " + country +".\n");
